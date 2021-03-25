6b6bc55c90a0118f9417f05333926dd597de0b82
Francisco Veloz <franciscoveloz245@gmail.com>
2021-03-25 10:55:47 -0600

    Insertamos pacientes

--- src/controllers/admin.paciente.controller.js
+++ src/controllers/admin.paciente.controller.js
@@ -1,10 +1,40 @@
 const pool = require('../lib/database')
 
 class PacienteController {
-    
-    Listar(req, res) {
-        const paciente = pool.query('select * from pacientes')
+
+    async Listar(req, res) {
+        const paciente = await pool.query('select * from pacientes')
         res.render('pacientes/pacientes', { paciente })
+    }
+
+    //Insertar usuarios
+    RenderAdd(req, res) {
+        res.render('pacientes/add')
+    }
+
+    async Insert(req, res) {
+        try {
+            const { nombre, apellidop, apellidom, edad, direccion, colonia, telefono } = req.body
+            const fecha = new Date()
+
+            const newPaciente = {
+                nombre,
+                apellidop,
+                apellidom,
+                edad,
+                direccion,
+                colonia,
+                telefono,
+                fecha
+            }
+
+            await pool.query('insert into pacientes set ?', [newPaciente])
+            req.flash('success', 'Paciente guardado con exito')
+            res.redirect('/admin/pacientes')
+        } catch (error) {
+            req.flash('message', 'No se pudo registar el paciente')
+            res.redirect('/admin/pacientes/add')
+        }
     }
 }
 
--- src/routes/admin.paciente.route.js
+++ src/routes/admin.paciente.route.js
@@ -4,5 +4,7 @@ const controller = require('../controllers/admin.paciente.controller')
 const { isLoggedIn, isAdminIn } =require('../lib/auth');
 
 router.get('/admin/pacientes', isLoggedIn, isAdminIn, controller.Listar)
+router.get('/admin/pacientes/add', isLoggedIn, isAdminIn, controller.RenderAdd)
+router.post('/admin/pacientes/add', isLoggedIn, isAdminIn, controller.Insert)
 
 module.exports = router
