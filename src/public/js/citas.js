b1c58a96ee36647209fe1669e43455d819875e8e
Francisco Veloz <franciscoveloz245@gmail.com>
2021-05-13 09:55:29 -0500

    Ultima vez con la vieja plantilla

--- src/public/js/citas.js
+++ src/public/js/citas.js
@@ -1,2 +1,36 @@
 const fecha = document.getElementById('fecha')
 const hora = document.getElementById('hora')
+
+const horarios = ['10:00', '10:50', '11:30', '12:20', '13:00']
+
+const API = 'http://localhost:5000'
+const API_CITAS = `${API}/api/citas`
+
+fecha.addEventListener('change', async () => {
+    hora.innerHTML = ''
+    hora.innerHTML += await getCitas(fecha);
+})
+
+const getCitas = async input => {
+    let r = ''
+    const response = await fetch(API_CITAS)
+    const citas = await response.json()
+    for (let i = 0; i < citas.length; i++) {
+        const cita = citas[i];
+        if (cita.fecha == input.value) {
+            for (let j = 0; j < horarios.length; j++) {
+                const horario = horarios[j]
+                if (cita.hora !== horario) {
+                    r += `<option>${horario}</option>`
+                }
+            }
+        } else {
+            for (let j = 0; j < horarios.length; j++) {
+                const horario = horarios[j];
+                r += `<option>${horario}</option>`
+            }
+        }
+    }
+
+    return r
+}
